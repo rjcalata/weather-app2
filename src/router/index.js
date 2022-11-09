82585489e1c67bab6c68d1233eea8bb1d4619098
Philip Mark Gutierrez <pipzgutz@gmail.com>
2022-11-09 09:36:04 +0800

    edited the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import About from '../views/About.vue';
 
 const routes = [
   {
@@ -13,11 +13,11 @@ const routes = [
     name: 'about',
     component: About,
   },
-]
+];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
-})
+});
 
-export default router
+export default router;
